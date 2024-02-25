63025940928b7593fa60a808cd3139c663e42d8f
Minh Khang <94234570+khanglag@users.noreply.github.com>
2024-02-25 21:35:39 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/coursemanager/CourseManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.coursemanager;
+
+/**
+ *
+ * @author khang
+ */
+public class CourseManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
