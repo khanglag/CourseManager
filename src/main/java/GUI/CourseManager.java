b726715d9b72a9726ef8c752622889c49fd5de66
Minh Khang <94234570+khanglag@users.noreply.github.com>
2024-02-25 23:38:01 +0700

    Create CourseManager.java

---
+++ src/main/java/GUI/CourseManager.java
@@ -0,0 +1,19 @@
+package GUI;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author khang
+ */
+public class CourseManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
