f2714135234497ac0da2e93ddfe3aaf008445e71
Minh Khang <94234570+khanglag@users.noreply.github.com>
2024-03-02 21:09:09 +0700

    t

--- src/main/java/GUI/course/tempCodeRunnerFile.java
+++
@@ -1,7 +0,0 @@
-package GUI.course;
-
- // // Đặt nút ở phía bên phải
-        // gbc.gridx = 1; // Đặt vị trí theo cột
-        // gbc.anchor = GridBagConstraints.LINE_END; // Đặt vị trí theo phía cuối
-        // gbc.weightx = 1.0; // Cho phép nút chiếm hết phần trống bên phải của JPanel
-        // panel.add(button, gbc);
